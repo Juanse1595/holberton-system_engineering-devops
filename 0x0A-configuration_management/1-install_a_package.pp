491f589487559fd5d69cc1c8579dcb1ae0c43922
Juanse1595 <3309@holbertonschool.com>
2022-01-03 22:53:32 +0000

    FIX 1rst adding provider

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Install a package with a puppet manifest
 package {
   'puppet-lint':
-  ensure => '2.5.0',
-  name   => 'puppet-lint',
-  source => 'https://rubygems.org/',
+  ensure   => '2.5.0',
+  name     => 'puppet-lint',
+  provider => 'gem',
 }
