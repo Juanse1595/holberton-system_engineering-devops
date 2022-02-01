e24037100bfe762eb67a6d69a0f6023960c687fc
Juanse1595 <3309@holberstonschool.com>
2022-01-31 23:22:22 -0500

    FIX 3rd

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -20,12 +20,14 @@ todos').json()
         employee_username = emp.get('username')
         '''Making the export dictionary'''
         for todo in todos:
-            subdict['username'] = employee_username
-            subdict['task'] = todo.get('title')
-            subdict['completed'] = todo.get('completed')
-            dict_list.append(subdict)
-            subdict = {}
+            if todo['userId'] == emp['id']:
+                subdict['username'] = employee_username
+                subdict['task'] = todo.get('title')
+                subdict['completed'] = todo.get('completed')
+                dict_list.append(subdict)
+                subdict = {}
         dict_export[emp['id']] = dict_list
+        dict_list = []
     '''Write to json file'''
     with open("todo_all_employees.json", "w") as f:
         json.dump(dict_export, f)
