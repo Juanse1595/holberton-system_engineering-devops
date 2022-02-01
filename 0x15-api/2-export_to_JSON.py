f90dc883995010eeede391474401eb302a4cae0c
Juanse1595 <3309@holberstonschool.com>
2022-01-31 21:38:30 -0500

    FIX 2 changing almost everything, using get method with the response.get result

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,25 +8,23 @@ import sys
 
 if __name__ == '__main__':
     given_id = int(sys.argv[1])
-    todos_data = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos = json.loads(todos_data.text)
-    employee_data = requests.get('https://jsonplaceholder.typicode.com/users')
-    employees = json.loads(employee_data.text)
+    todos = requests.get('https://jsonplaceholder.typicode.com/\
+todos?userId={}'.format(given_id)).json()
+    employees = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                                 .format(given_id)).json()
     '''Creating the subdictionaries, list and dictionary to export'''
     subdict = {}
     dict_list = []
     dict_export = {}
     '''Getting employee username'''
-    for empl in employees:
-        if empl['id'] == given_id:
-            employee_username = empl['username']
+    employee_username = employees.get('username')
+    '''Making the export dictionary'''
+    for todo in todos:
+        subdict['task'] = todo.get('title')
+        subdict['completed'] = todo.get('completed')
+        subdict['username'] = employee_username
+        dict_list.append(subdict)
+    dict_export[given_id] = dict_list
     '''Write to json file'''
     with open("{}.json".format(given_id), "w") as f:
-        for todo in todos:
-            if todo['userId'] == given_id:
-                subdict['task'] = todo['title']
-                subdict['completed'] = todo['completed']
-                subdict['username'] = employee_username
-                dict_list.append(subdict)
-        dict_export[str(given_id)] = dict_list
         json.dump(dict_export, f)
