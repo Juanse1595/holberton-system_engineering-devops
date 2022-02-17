c521f2378ee8ebd630a32f7bd005648ad7a830db
Juanse1595 <3309@holberstonschool.com>
2022-02-17 16:33:48 -0500

    fix style

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,6 @@
 # This script fixes the web server changing a line in a config file
 
-include stdlib
-exec {
-    'fix line on wp-settings.php':
-    onlyif  => 'test -e /var/www/html/wp-settings.php'
-    command => "sed -i 's/phpp/php' /var/www/html/wp-settings.php",
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+exec {'fix':
+      command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+      path    => '/bin/',
 }
