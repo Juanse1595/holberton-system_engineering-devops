f7c7b4aa2d89ed2232f1ebb459c8c7472c09f218
Juanse1595 <3309@holberstonschool.com>
2022-02-24 23:00:30 -0500

    ADD last project dir with files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Config file for debugging task
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+# Restart Nginx
+exec { 'Restart_Nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
